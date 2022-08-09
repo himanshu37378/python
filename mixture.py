d333afac3629d3e6119fb8598f1aa0b09e3f042f
himanshu37378 <77109898+himanshu37378@users.noreply.github.com>
2022-08-09 23:03:25 +0530

    Delete mixture.py

--- mixture.py
+++
@@ -1,9 +0,0 @@
-a = int(input("enetr the units of solid"))
-b = int(input("enetr the units of liquid"))
-
-if( a > 0 and b > 0):
-    print("Solution")
-elif(a == 0 and b > 0 ):
-    print("liquid")
-elif( a > 0 and b == 0):
-    print("solid")
